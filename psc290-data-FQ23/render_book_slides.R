81e5e0c0533cd19f32c45a1d7033101fccb0c57c
edbeck@wustl.edu <edbeck@wustl.edu>
2023-09-30 14:08:42 -0700

    restructure main

--- psc290-data-FQ23/render_book_slides.R
+++
@@ -1,8 +0,0 @@
-render_book_slides <- function(...) {
-  quarto::quarto_render(..., as_job = FALSE)
-  file.rename("_quarto.yml", "_quarto_site.yml")
-  file.rename("_quarto_revealjs.yml", "_quarto.yml")
-  on.exit(file.rename("_quarto.yml", "_quarto_revealjs.yml"))
-  on.exit(file.rename("_quarto_site.yml", "_quarto.yml"), add = TRUE)
-  quarto::quarto_render(..., as_job = FALSE)
-}
